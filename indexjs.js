cf17569a5d67c1fd7ac5022fd7d8aa8c603d17b3
Jevdev2304 <jvaldes245@soyudemedellin.edu.co>
2023-01-01 10:43:48 -0500

    Repaso de lectura del DOM desde JS e introducción de Escritura en el DOM

--- indexjs.js
+++ indexjs.js
@@ -8,3 +8,28 @@ console.log({h1, p, parrafito, pid, input,
 
 });
 console.log(input.value);
+h1.innerText = "Gatito Blanco :D";
+p[0].innerText = "Soy un Gatito blanco y me gusta jugar con bolas de lana.";
+p[1].innerText = "Como me llaman ?";
+parrafito.innerText = "Me llaman el destrozaPesebres3000 xd."
+
+
+
+//console.log(h1.getAttribute("class"));
+//h1.setAttribute("class", "rojo");
+h1.classList.add("rojo");
+h1.classList.remove("verde");
+input.placeholder="Ingresa mi nombre UwU"
+input.value="Copito";
+const img = document.createElement("img");
+pid.innerHTML = "";
+pid.append(img);
+
+img.style.width ="400px";
+img.style.height="400px";
+img.style.borderRadius = "16px";
+img.style.marginLeft = "0px"
+img.style.bottom = "0px";
+img.style.marginTop = "16px";
+img.setAttribute("src","https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+
